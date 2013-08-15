0b0f6cb3abc60edda24ada91304fb27dd493272c
Craig Payne <craigpayne1985@googlemail.com>
2013-08-15 07:07:51 +0100

    final commit

---
+++ Api.js
@@ -0,0 +1,13 @@
+﻿exports.Api = Api;
+
+function Api() { }
+Api.prototype.controller = new Controller();
+function Controller() {}
+Controller.prototype.GetAction = function(request, response) {
+    response.setHeader('content-type', 'application/json');
+    response.send(500, "This is a reserved method, please call the method using a controller name and a action rather than what you just did.");
+};
+Controller.prototype.PostAction = function (request, response) {
+    response.setHeader('content-type', 'application/json');
+    response.send(500, "This is a reserved method, please call the method using a controller name and a action rather than what you just did.");
+};
---
+++ server.js
@@ -0,0 +1,26 @@
+﻿var restify = require('restify');
+var server = restify.createServer({ name: 'TripleTriadApi' });
+var apiModule = require('./Api.js');
+var api = new apiModule.Api();
+
+server.get('/:controller/:action', function (request, response) {
+    if (api[request.params.controller] == undefined || api[request.params.controller]["Get" + request.params.action] == undefined) {
+        response.setHeader('content-type', 'application/json');
+        response.send(404, "Content does not exist");
+    }else {
+        api[request.params.controller]["Get" + request.params.action](request, response);
+    }
+});
+server.post('/:controller/:action', function (request, response) {
+    if (api[request.params.controller] == undefined || api[request.params.controller]["Post" + request.params.action] == undefined) {
+        response.setHeader('content-type', 'application/json');
+        response.send(404, "Content does not exist");
+    } else {
+        api[request.params.controller]["Post" + request.params.action](request, response);
+    }
+});
+
+server.use(restify.fullResponse()).use(restify.bodyParser());
+server.listen(8081, function () {
+    console.log('%s listening at %s', server.name, server.url);
+});
