26ccf9abc73a091aa40acbb0c1da8be75d5692a4
Craig Payne <craigpayne1985@googlemail.com>
2013-08-16 18:38:28 +0100

    Updated code to allow case insensitivity
    Updated code to allow case insensitivity for requests

--- server.js
+++ server.js
@@ -1,26 +1,70 @@
 ﻿var restify = require('restify');
 var server = restify.createServer({ name: 'TripleTriadApi' });
-var apiModule = require('./Api.js');
-var api = new apiModule.Api();
+Setup(controllerNotFound, actionNotFound);
 
-server.get('/:controller/:action', function (request, response) {
-    if (api[request.params.controller] == undefined || api[request.params.controller]["Get" + request.params.action] == undefined) {
-        response.setHeader('content-type', 'application/json');
-        response.send(404, "Content does not exist");
-    }else {
-        api[request.params.controller]["Get" + request.params.action](request, response);
+function Api() { }
+var api = new Api();
+
+/*  add to the Api prototype to add new methods.
+example: the following method will be called from /api/controller/action when using a get*/
+Api.prototype.Controller = {
+    GetAction: function (req, res) {
+        res.send("Default action on controller");
     }
-});
-server.post('/:controller/:action', function (request, response) {
-    if (api[request.params.controller] == undefined || api[request.params.controller]["Post" + request.params.action] == undefined) {
-        response.setHeader('content-type', 'application/json');
-        response.send(404, "Content does not exist");
-    } else {
-        api[request.params.controller]["Post" + request.params.action](request, response);
+};
+
+
+function controllerNotFound(req, res) {
+    res.send("Controller Not Found!");
+}
+function actionNotFound(req, res) {
+    res.send("Controller Not Found!");
+}
+
+function Setup(methodTypes, notFoundControllerHandler, notFoundActionHandler){
+    
+    server.get("/api/:controller/:action", getResponseHandler);
+    server.post("/api/:controller/:action", postResponseHandler);
+    server.put("/api/:controller/:action", putResponseHandler);
+    server.del("/api/:controller/:action", deleteResponseHandler);
+
+    function getResponseHandler(req, res) {responseHandler(req, res, "get");}
+    function postResponseHandler(req, res) { responseHandler(req, res, "post"); }
+    function putResponseHandler(req, res) { responseHandler(req, res, "put"); }    
+    function deleteResponseHandler(req, res) { responseHandler(req, res, "delete"); }
+
+    function responseHandler(request, response, method) {
+        var handle = {};
+        try {
+            handle = api.FindRoute(request.params.controller, request.params.action, method);
+        } catch (exception) {
+            if (exception == "Controller not found") {
+                notFoundControllerHandler(request, response);
+            } else if (exception == "Action not found") {
+                notFoundActionHandler(request, response);
+            } else {
+                throw "Unknown Error";
+            }
+        }
+        handle(request, response);
+    };
+    
+}
+
+Api.prototype.FindRoute = function(controllerName, actionName, method) {
+    //find matching controller and action (without case sensitivity)
+    for (var controller in api) {
+        if (controller.toLowerCase() === controllerName.toLowerCase()) {
+            for (var action in api[controller]) {
+                if (action.toLowerCase() === method + actionName.toLowerCase()) {
+                    return api[controller][action];
+                }
+            }
+            throw "Action not found";
+        }
     }
-});
+    throw "Controller not found";
+};
 
 server.use(restify.fullResponse()).use(restify.bodyParser());
-server.listen(8081, function () {
-    console.log('%s listening at %s', server.name, server.url);
-});
+server.listen(8081, function () { console.log('%s listening at %s', server.name, server.url); });
