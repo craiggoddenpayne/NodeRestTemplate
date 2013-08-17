39325b778fb8d76fd0dc6e0f761b2af7da8040e9
Craig Payne <craigpayne1985@googlemail.com>
2013-08-17 09:01:45 +0100

    fixed a few issues

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 ﻿var restify = require('restify');
-var server = restify.createServer({ name: 'TripleTriadApi' });
+var server = restify.createServer({ name: 'WebServer' });
 var testController = require("./Controllers/Test.js");
 
 var api = new MvcApi();
@@ -17,20 +17,39 @@ function actionNotFound(req, res) {
 Setup(controllerNotFound, actionNotFound);
 function Setup(notFoundControllerHandler, notFoundActionHandler) {
     
+
+    server.get("/api/:controller", getResponseHandler);
+    server.post("/api/:controller", postResponseHandler);
+    server.put("/api/:controller", putResponseHandler);
+    server.del("/api/:controller", deleteResponseHandler);
+    server.head("/api/:controller", headResponseHandler);
+    server.opts("/api/:controller", optionsResponseHandler);
+    server.patch("/api/:controller", patchResponseHandler);
     server.get("/api/:controller/:action", getResponseHandler);
     server.post("/api/:controller/:action", postResponseHandler);
     server.put("/api/:controller/:action", putResponseHandler);
     server.del("/api/:controller/:action", deleteResponseHandler);
+    server.head("/api/:controller/:action", headResponseHandler);
+    server.opts("/api/:controller/:action", optionsResponseHandler);
+    server.patch("/api/:controller/:action", patchResponseHandler);
 
     function getResponseHandler(req, res) {responseHandler(req, res, "get");}
     function postResponseHandler(req, res) { responseHandler(req, res, "post"); }
     function putResponseHandler(req, res) { responseHandler(req, res, "put"); }    
     function deleteResponseHandler(req, res) { responseHandler(req, res, "delete"); }
-
+    function headResponseHandler(req, res) { responseHandler(req, res, "head"); }
+    function optionsResponseHandler(req, res) { responseHandler(req, res, "options"); }
+    function patchResponseHandler(req, res) { responseHandler(req, res, "patch"); }
+    
     function responseHandler(request, response, method) {
         var handle = {};
         try {
-            handle = api.FindRoute(request.params.controller, request.params.action, method);
+            var controller = request.params.controller;
+            var action = request.params.action;
+            if (!controller) controller = "";
+            if (!action) action = "";
+
+            handle = api.FindRoute(controller, action, method);
         } catch (exception) {
             if (exception == "Controller not found") {
                 notFoundControllerHandler(request, response);
@@ -50,7 +69,7 @@ MvcApi.prototype.FindRoute = function (controllerName, actionName, method) {
     for (var controller in api) {
         if (controller.toLowerCase() === controllerName.toLowerCase()) {
             for (var action in api[controller]) {
-                if (action.toLowerCase() === method + actionName.toLowerCase()) {
+                if (action.toLowerCase() === (method + actionName.toLowerCase())) {
                     return api[controller][action];
                 }
             }
